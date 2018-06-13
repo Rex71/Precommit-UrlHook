2820554f685ae621b4a22f0ca152ffce0f5df31d
Rex71 <ezekiel7176@gmail.com>
2018-06-13 21:39:08 +0200

    modified stash

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #Run your pre-commit check from here for date changes
 # ./urlcheck-run.sh
-git stash -q --keep-index
-./urlcheck-run.sh
+#git stash -q --keep-index
+./urlcheck-run.sh "$@"
 RESULT=$?
-git stash pop -q
+#git stash pop -q
 [ $RESULT -ne 0 ] && exit 1
 exit 0
