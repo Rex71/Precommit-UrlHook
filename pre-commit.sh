c2216174fb212e9c24bea6a7f5925ea349ad589a
Rex71 <ezekiel7176@gmail.com>
2018-06-13 22:11:40 +0200

    some changes

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,7 +2,8 @@
 #Run your pre-commit check from here for date changes
 # ./urlcheck-run.sh
 #git stash -q --keep-index
-./urlcheck-run.sh "$@"
+#./urlcheck-run.sh "$@" #to pass arguments
+./urlcheck-run.sh www.google.com
 RESULT=$?
 #git stash pop -q
 [ $RESULT -ne 0 ] && exit 1
